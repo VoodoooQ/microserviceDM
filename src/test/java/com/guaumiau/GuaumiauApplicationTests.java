4d2dd34e19cf326eea211ae209c7590c3f795afa
VoodoooQ <maximi.diazc@duocuc.cl>
2025-11-28 09:26:14 -0300

    ContextLoads test con h2 para pruebas

--- src/test/java/com/guaumiau/GuaumiauApplicationTests.java
+++ src/test/java/com/guaumiau/GuaumiauApplicationTests.java
@@ -2,8 +2,10 @@ package com.guaumiau;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class GuaumiauApplicationTests {
 
 	@Test
